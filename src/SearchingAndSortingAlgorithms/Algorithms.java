4dc7bbae453495c5b3f7b25a643867985de73aee
JackChine27 <league@WTS14.attlocal.net>
2017-09-27 18:25:44 -0700

    tests

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -4,7 +4,54 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0; //<- this needs changing
+		int bleh = 0;
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i).equals("whole")) {
+				bleh += 1;
+			} else {
+				return bleh;
+			}
+		}
+		return 1;
+	}
+	
+	public static int countPearls(List<Boolean> oysters) {
+		int bleh = 0;
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i)) {
+				bleh += 1;
+			}
+		}
+		return bleh;
+	}
+	
+	public static double findTallest(List<Double> peeps) {
+		double tallest = 0.0;
+		for (int i = 0; i < peeps.size(); i++) {
+			if (peeps.get(i) > tallest) {
+				tallest = peeps.get(i);
+			}
+		}
+		return tallest;
+	}
+	
+	public static String findLongestWord(List<String> words) {
+		String longest = "hi";
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).length() > longest.length()) {
+				longest = words.get(i);
+			}
+		}
+		return longest;
+	}
+	
+	public static boolean containsSOS(List<String> messages) {
+		for (int i = 0; i < messages.size(); i++) {
+			if (messages.get(i).contains("... --- ...")) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	//Add other methods here
